2a7e14b9d19deb415dd32b695ca2bd625f17b682
Kru06 <89855611+Kru06@users.noreply.github.com>
2022-09-19 13:48:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import NavigationBar from './Component/NavigationBar';
+import Footer from './Component/Footer';
+import ViewStudents from './Component/ViewStudents';
+import { Container } from 'react-bootstrap';
+import Student from './Component/Student';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <NavigationBar/>
+      <Container>
+        <Routes>
+        <Route path="/addStudent" element={<Student/>}/>
+        <Route path="/updateStudent/:studentId" element={<Student/>}/>
+        {<Route path="/viewStudents" element={<ViewStudents/>}/> }
+        </Routes>
+      </Container>
+      </BrowserRouter>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
